26b972e6d9adce650b14a5f127baad764fcebca6
Salman Ahmad <sheikhsalmanahmedofficial@gmail.com>
2026-04-15 19:14:09 +0500

    Update server.js

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,9 +1,11 @@
 import express from "express";
 import dotenv from "dotenv";
 const app = express();
-const PORT = 5001;
+const PORT = process.env.PORT || 5000;
 import {connectDB} from "./config/db.js";
 import notesRoutes from "./routes/notesRoutes.js";
+import dns  from "dns";
+dns.setServers(["8.8.8.8", "8.8.4.4"]);
 dotenv.config();
 connectDB();
 app.use("/api/notes", notesRoutes);
